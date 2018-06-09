63f91e091623b0cbfe2862cba42d52c6017922c6
Marco Milani <emme.milani@hotmail.it>
2018-06-09 23:31:54 +0200

    Update to bridge 17 Add Partial Model Add IViewModelLifeCycle Add LoadableViewModelWithSubs

---
+++ Bridge.Spaf/Bridge.Spaf/PartialModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Bridge.jQuery2;
+
+namespace Bridge.Spaf
+{
+    public abstract class PartialModel : ViewModelBase, IViewModelLifeCycle
+    {
+        protected abstract string HtmlUrl { get; }
+
+        protected virtual Action OnPostInit { get; } = null;
+        protected virtual Action OnPreDeInit { get; } = null;
+
+
+        public void Init()
+        {
+            jQuery.Select($"#{this.ElementId()}").Load(HtmlUrl, null, (o, s, arg3) =>
+            {
+                base.ApplyBindings();
+            });
+            
+            OnPostInit?.Invoke();
+        }
+
+        public void DeInit()
+        {
+            OnPreDeInit?.Invoke();
+            base.RemoveBindings();
+        }
+    }
+
+    public interface IViewModelLifeCycle
+    {
+        void Init();
+        void DeInit();
+    }
+}
+
+
+
