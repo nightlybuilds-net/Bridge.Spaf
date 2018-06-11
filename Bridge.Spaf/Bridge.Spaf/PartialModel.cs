8b20a407eeac2eaa85148dcc4ea5c8abb0ff0216
Marco Milani <emme.milani@hotmail.it>
2018-06-11 09:14:49 +0200

    removed action on abstract class

--- Bridge.Spaf/Bridge.Spaf/PartialModel.cs
+++ Bridge.Spaf/Bridge.Spaf/PartialModel.cs
@@ -7,23 +7,16 @@ namespace Bridge.Spaf
     {
         protected abstract string HtmlUrl { get; }
 
-        protected virtual Action OnPostInit { get; } = null;
-        protected virtual Action OnPreDeInit { get; } = null;
-
-
         public void Init()
         {
             jQuery.Select($"#{this.ElementId()}").Load(HtmlUrl, null, (o, s, arg3) =>
             {
                 base.ApplyBindings();
             });
-            
-            OnPostInit?.Invoke();
         }
 
         public void DeInit()
         {
-            OnPreDeInit?.Invoke();
             base.RemoveBindings();
         }
     }
